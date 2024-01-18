01a64816406bc68c854d0f1bed710ae955d3fc4b
oseongryu <oseongryu@gmail.com>
2024-01-18 16:10:03 +0900

    feat: automation

--- 06_automation/mv_war.sh
+++ 06_automation/mv_war.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cp -r ~/git-personal/spring-boot-jsp/drawing-app/build/libs/drawing-app.war ~/git-personal/docker-composes/06_automation/was/static
+cp -r ~/git/spring-boot-jsp/drawing-app/build/libs/drawing-app.war ~/git/docker-composes/06_automation/was/static
