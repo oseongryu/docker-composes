df92cd1b0e3136ea30a961a78ed8ff6c5e5459b3
oseongryu <oseongryu@gmail.com>
2024-02-22 16:19:03 +0900

    fix: script

--- automation/python/script/a_init_ubuntu.sh
+++ automation/python/script/a_init_ubuntu.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 ls -al
 echo "init ubuntu"
-# apt -y update
-# apt -y upgrade
-# apt -y install python3-pip
-# apt -y install python3-tk
-# apt -y install python3-dev 
-# apt -y install python-tk
+apt -y update
+apt -y upgrade
+apt -y install python3-pip
+apt -y install python3-tk
+apt -y install python3-dev 
+apt -y install python-tk
 
-# apt -y install xvfb
-# apt -y install scrot
+apt -y install xvfb
+apt -y install scrot
 
-# apt -y install fonts-indic
-# apt -y install font-noto
-# apt -y install fonts-noto-cjk
+apt -y install fonts-indic
+apt -y install font-noto
+apt -y install fonts-noto-cjk
 
 timedatectl set-timezone Asia/Seoul
 # date
