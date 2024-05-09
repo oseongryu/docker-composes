2b4ff57c937f01e68145201f03259229b33c2815
oseongryu <oseongryu@gmail.com>
2024-05-10 00:07:25 +0900

    fix: ubuntu project setting

--- ubuntu/script/init.sh
+++ ubuntu/script/init.sh
@@ -1,7 +1,12 @@
+# timezone
+timedatectl set-timezone Asia/Seoul
+
 cd /app
 tar -zxvf code-stable-x64-1714529314.tar.gz
 tar -zxvf ideaIU-2024.1.1.tar.gz
-# mv /app/.ssh ~/.ssh
+# cp -r /app/.ssh ~/.ssh
+chmod 600 ~/.ssh/id_rsa
 
-sh /app/script/init_user.sh
-sh /app/script/init_korean.sh
+# mkdir /app/VSCode-linux-x64/data
+# cp -r /app/VSCode-linux-x64 /usr/share/code
+# chmod 777 /user/share/code
--- ubuntu/script/init_user.sh
+++ ubuntu/script/init_user.sh
@@ -1,11 +1,9 @@
 # sudo adduser ubuntu #password: password
 
-
 USERNAME=ubuntu
 USER_UID=1000
 USER_GID=$USER_UID
 USER_PASSWD=password
-
 
 groupadd --gid $USER_GID $USERNAME \
     && useradd --uid $USER_UID --gid $USER_GID -m $USERNAME \
