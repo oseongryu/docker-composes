19d96eba19a90a75ac8a10358d9775469480084e
oseongryu <oseongryu@gmail.com>
2024-05-10 17:57:11 +0900

    fix: ubuntu-desktop android studio

--- ubuntu/static/script/app_android_studio.sh
+++ ubuntu/static/script/app_android_studio.sh
@@ -17,3 +17,26 @@ sudo echo "Name[en_GB]=android-studio.desktop" >> /usr/share/applications/androi
 # mod permissions
 sudo chmod 644 /usr/share/applications/android-studio.desktop
 sudo chown root:root /usr/share/applications/android-studio.desktop
+
+
+
+# # https://dev.to/janetmutua/installing-android-studio-on-ubuntu-2204-complete-guide-1kh8
+# wget https://dl.google.com/dl/android/studio/ide-zips/2022.1.1.21/android-studio-2022.1.1.21-linux.tar.gz
+# tar -zxvf android-studio-2022.1.1.21-linux.tar.gz
+# sudo mv android-studio /opt/
+# sudo ln -sf /opt/android-studio/bin/studio.sh /bin/android-studio
+
+# sudo nano /usr/share/applications/android-studio.desktop
+
+# [Desktop Entry]
+# Version=1.0
+# Type=Application
+# Name=Android Studio
+# Comment=Android Studio
+# Exec=bash -i "/opt/android-studio/bin/studio.sh" %f
+# Icon=/opt/android-studio/bin/studio.png
+# Categories=Development;IDE;
+# Terminal=false
+# StartupNotify=true
+# StartupWMClass=jetbrains-android-studio
+# Name[en_GB]=android-studio.desktop
