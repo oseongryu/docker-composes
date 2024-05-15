b47ef14ad3896c9282a8ad4985fe182a6c3e7ac7
oseongryu <oseongryu@gmail.com>
2024-05-15 23:02:54 +0900

    fix: warp

--- ubuntu/static/script/app_warp.sh
+++ ubuntu/static/script/app_warp.sh
@@ -7,3 +7,14 @@ sudo apt-get update && sudo apt-get install cloudflare-warp
 
 # warp-cli register
 # warp-cli connect
+# warp-cli disconnect
+
+curl https://www.cloudflare.com/cdn-cgi/trace/
+
+### brave browser
+
+sudo apt install curl
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
