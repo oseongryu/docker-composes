11c47850b5caaac0bc21c6df32fd00ea6e0f14b5
oseongryu <oseongryu@gmail.com>
2024-06-05 00:42:52 +0900

    docs: fix

--- nuxt-vue/spring/script/start_was.sh
+++ nuxt-vue/spring/script/start_was.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-nohup /app/java/zulu8.78.0.19-ca-jdk8.0.412-linux_x64/bin/java -jar -Dserver.port=9551 /app/script/fo-0.0.1-SNAPSHOT.jar 1>/dev/null 2>&1 &
+# nohup /app/java/zulu8.78.0.19-ca-jdk8.0.412-linux_x64/bin/java -jar -Dserver.port=9551 /app/script/fo-0.0.1-SNAPSHOT.jar 1>/dev/null 2>&1 &
+nohup /app/java/zulu8.78.0.19-ca-jdk8.0.412-linux_x64/bin/java -jar -Dserver.port=9551 /app/script/fo-0.0.1-SNAPSHOT.jar 1> temp.log 2>&1 &
 echo "start_was"
