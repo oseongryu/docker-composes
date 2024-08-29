a7bdc814ad0970629e6d3336afc7abed6958a064
oseongryu <oseongryu@gmail.com>
2024-08-29 21:32:31 +0900

    fix: intellij

--- static/script/app_intellij.sh
+++ static/script/app_intellij.sh
@@ -1,3 +1,23 @@
+#!/bin/bash
+sh_result=$(uname -m)
+cd /app && sudo tar -zxvf ideaIU-*.tar.gz
+
+if [ "$sh_result" = "aarch64" ]; then
+    app_name=idea-IU-241.17890.1
+else 
+    app_name=idea-IU-241.17890.1
+fi
+
+if [ "$sh_result" = "aarch64" ]; then
+    sudo chown root:root /app/$app_name
+    sudo chmod 777 /app/$app_name
+    sudo mv /app/$app_name /opt/
+else 
+    sudo chown root:root /app/$app_name
+    sudo chmod 777 /app/$app_name
+    sudo mv /app/$app_name /opt/
+fi
+
 # sudo adduser ubuntu
 # echo "xfce4-session" > ~/.xsession
 # echo ubuntu ALL=\(root\) NOPASSWD:ALL > /etc/sudoers.d/ubuntu
@@ -5,30 +25,28 @@
 # https://gist.github.com/rob-murray/6828864
 # create file:
 sudo touch /usr/share/applications/intellij.desktop
+sudo chown root:root /usr/share/applications/intellij.desktop
+sudo chmod 777 /usr/share/applications/intellij.desktop
 
 sudo echo "[Desktop Entry]" >> /usr/share/applications/intellij.desktop
 sudo echo "Version=13.0" >> /usr/share/applications/intellij.desktop
 sudo echo "Type=Application" >> /usr/share/applications/intellij.desktop
 sudo echo "Terminal=false" >> /usr/share/applications/intellij.desktop
-sudo echo "Icon[en_US]=/app/idea-IU-241.15989.150/bin/idea.png" >> /usr/share/applications/intellij.desktop
+sudo echo "Icon[en_US]=/opt/$app_name/bin/idea.png" >> /usr/share/applications/intellij.desktop
 sudo echo "Name[en_US]=IntelliJ" >> /usr/share/applications/intellij.desktop
-sudo echo "Exec=/app/idea-IU-241.15989.150/bin/idea.sh" >> /usr/share/applications/intellij.desktop
+sudo echo "Exec=/opt/$app_name/bin/idea.sh" >> /usr/share/applications/intellij.desktop
 sudo echo "Name=IntelliJ" >> /usr/share/applications/intellij.desktop
-sudo echo "Icon=/app/idea-IU-241.15989.150/bin/idea.png" >> /usr/share/applications/intellij.desktop
+sudo echo "Icon=/opt/$app_name/bin/idea.png" >> /usr/share/applications/intellij.desktop
 sudo echo "Categories=TextEditor;Development;IDE;" >> /usr/share/applications/intellij.desktop
-
-# mod permissions
-sudo chmod 644 /usr/share/applications/intellij.desktop
-sudo chown root:root /usr/share/applications/intellij.desktop
 
 # # intellij
 # [Desktop Entry]
 # Version=13.0
 # Type=Application
 # Terminal=false
-# Icon[en_US]=/app/idea-IU-241.15989.150/bin/idea.png
+# Icon[en_US]=/opt/$app_name/bin/idea.png
 # Name[en_US]=IntelliJ
-# Exec=/app/idea-IU-241.15989.150/bin/idea.sh
+# Exec=/opt/$app_name/bin/idea.sh
 # Name=IntelliJ
-# Icon=/app/idea-IU-241.15989.150/bin/idea.png
+# Icon=/opt/$app_name/bin/idea.png
 # Categories=TextEditor;Development;IDE;
