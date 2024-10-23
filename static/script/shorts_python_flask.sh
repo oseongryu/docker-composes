cdbd4e4c54c7ee3fc9ddb1c0a3ed9f90c5a99277
oseongryu <oseongryu@gmail.com>
2024-10-23 15:47:16 +0900

    fix: shorts

---
+++ static/script/shorts_python_flask.sh
@@ -0,0 +1,8 @@
+apt-get install -y pkg-config python-dev default-libmysqlclient-dev libssl-dev 
+
+pip install --upgrade pip
+pip install Flask
+pip install Flask-MySQLdb
+pip install python-dotenv
+pip install mysql-connector-python
+pip install gTTS
